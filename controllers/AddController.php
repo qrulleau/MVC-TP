29a995e5f909233d816878714dbfda41b47ee224
qrulleau <quentin.rulleau@yahoo.com>
2023-10-20 23:34:41 +0200

    ADD: check username not in password in the registration

--- controllers/AddController.php
+++ controllers/AddController.php
@@ -1,8 +1,12 @@
 <?php
 
+session_start();
+
+if (!isset($_SESSION['username'])){
+  header('Location : http://www.localhost/MVC-TP/ ');
+}
+
 require_once __DIR__ . '/../models/movies.php';
-
-
 
 function test (){
   global $title,$title,$producer;
--- controllers/SignInController.php
+++ controllers/SignInController.php
@@ -4,30 +4,29 @@ if (session_status() == PHP_SESSION_NONE) {
   session_start();
 }
 
-
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
-
 require_once __DIR__ . '/../models/user.php';
 global $errors;
-// $_SESSION['errors'] = " ";
+$_SESSION['errors'] = " ";
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 if (isset($username)){
   $regex = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W\_]).{8,}$/';
-  $result = preg_match($regex, $password);
-  if ($result) {
-    
+  $checkRegex = preg_match($regex, $password);
+  $checkPassword = strpos($password,$username);
+  // var_dump($password);
+  // var_dump($username);
+  // var_dump($checkPassword);
+  // die();
+  if ($checkRegex && $checkPassword === false) {
     User::createUser($password, $email, $username);
     header('Location: http://www.localhost/MVC-TP/');
     exit();
   } else {
-      // $_SESSION['errors'] = "Votre mot de passe n'est pas valide";
-      $errors = "Votre mot de passe n'est pas valide";
+      $_SESSION['errors'] = "Votre mot de passe n'est pas valide";
   }
   
 }
+
 require_once __DIR__ . '/../views/signin.php';
